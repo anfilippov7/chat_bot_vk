df307fe70f5f9174e6bf8a912b051b4353bd97b4
anfilippov7 <106445819+anfilippov7@users.noreply.github.com>
2022-09-30 09:14:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,31 @@
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+from models import create_tables
 
-import requests
-from pprint import pprint
-from VK import VK
-from datetime import datetime
-# import json
+load_dotenv()
 
-def age ():   ## Метод вычисляет возраст
-    info = vk.users_info()
-    date_now = datetime.today()
-    day_now = date_now.day
-    month_now = date_now.month
-    year_now = date_now.year
-    user_bdate = info['response'][0]['bdate']
-    day_user = int(user_bdate.split('.')[0])
-    month_user = int(user_bdate.split('.')[1])
-    year_user = int(user_bdate.split('.')[2])
+# Предварительно нужно создать базу данных в командной строке операционной системы командой: createdb -U postgres chat_bot_vk
 
-    user_age = year_now - year_user
-    if month_now < month_user:
-        user_age -= 1
-    elif month_now == month_user and day_now < day_user:
-        user_age -= 1
-    return user_age
+# DSN = 'postgresql://postgres:1234@localhost:5432/chat_bot_vk'
 
-if __name__ == '__main__':
+# В переменной окружения операционной системы прописываем переменную DSN со значением 'postgresql://postgres:1234@localhost:5432/chat_bot_vk'
+# где 1234 это пароль от базы данных (у каждого разработчика может отличаться)
 
-#Считываем токен для доступа в API_vk из файла 'token_vk.txt'
-    with open('token_vk.txt', "r") as file_object:
-        token_vk = file_object.read().strip()
-#Считываем ID пользователя vk из файла 'ID_vk.txt'
-    with open('ID_vk.txt', "r") as file_object:
-        ID_vk = file_object.read().strip()
+DSN = os.getenv("DSN")
 
-    vk = VK(token_vk, ID_vk)
-    qty_photos = vk.users_photos()['response']['count']
-    print(f"У пользователя {qty_photos} фотографий")
-    print(f'Возраст пользователя {age()} лет')
-    pprint(vk.users_info())
+engine = sqlalchemy.create_engine(DSN)
+create_tables(engine)
 
-    dict_photos = vk.users_photos()['response']['items']
-    print("Информация о фотографиях получена")
-    pprint(vk.get_list_photos(dict_photos=dict_photos, qty_photos=qty_photos))
+Session = sessionmaker(bind=engine)
+session = Session()
+
+session.close()
+
+
+# if __name__ == '__main__':
+
+
+
 
