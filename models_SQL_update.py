c792908a585ea00a80732e9b32cddedbe0d96348
anfilippov7 <106445819+anfilippov7@users.noreply.github.com>
2022-10-05 17:39:49 +0300

    Add files via upload

---
+++ models_SQL_update.py
@@ -0,0 +1,34 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+
+class People(Base):
+    __tablename__ = "people"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    first_name = sq.Column(sq.String(length=40), nullable=False)
+    last_name = sq.Column(sq.String(length=40), nullable=False)
+    profile_link = sq.Column(sq.String(length=60), nullable=False)
+    first_likes = sq.Column(sq.String(length=60), nullable=False)
+    second_likes = sq.Column(sq.String(length=60), nullable=False)
+    third_likes = sq.Column(sq.String(length=60), nullable=False)
+
+class Blacklist(Base):
+    __tablename__ = "blacklist"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_people = sq.Column(sq.Integer, sq.ForeignKey("people.id"), nullable=False)
+    people = relationship(People, backref="blacklists")
+
+class Favourite(Base):
+    __tablename__ = "favourite"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_people = sq.Column(sq.Integer, sq.ForeignKey("people.id"), nullable=False)
+    people = relationship(People, backref="favourites")
+
+
+def create_tables(engine):
+    Base.metadata.create_all(engine)
