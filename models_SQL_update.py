eb9068ddd543e451743092720bf5377df9c9cabe
anfilippov7 <106445819+anfilippov7@users.noreply.github.com>
2022-10-11 14:26:51 +0300

    Add files via upload

--- models_SQL_update.py
+++ models_SQL_update.py
@@ -7,23 +7,21 @@ Base = declarative_base()
 class People(Base):
     __tablename__ = "people"
 
-    id = sq.Column(sq.Integer, primary_key=True) #id
-    auser_id = sq.Column(sq.Integer, nullable=False) #id человека которыый ищет
-    search_vk_id = sq.Column(sq.Integer, nullable=False) #id людей которых мы ищем
+    id = sq.Column(sq.Integer, primary_key=True)
+    auser_id = sq.Column(sq.Integer, nullable=False)
+    search_vk_id = sq.Column(sq.Integer, nullable=False)
     first_name = sq.Column(sq.String(length=40), nullable=False)
     last_name = sq.Column(sq.String(length=40), nullable=False)
     profile_link = sq.Column(sq.String(length=60), nullable=False)
-    first_likes = sq.Column(sq.String(length=60), nullable=False)
-    second_likes = sq.Column(sq.String(length=60), nullable=False)
-    third_likes = sq.Column(sq.String(length=60), nullable=False)
+    photos_likes = sq.Column(sq.String(length=100), nullable=False)
+
 
 class Blacklist(Base):
     __tablename__ = "blacklist"
 
-    id = sq.Column(sq.Integer, primary_key=True) #id
-    auser_id = sq.Column(sq.Integer, nullable=False) #id человека которыый ищет
-    search_vk_id = sq.Column(sq.Integer, nullable=False) #id людей которых мы ищем
-    # search_vk_id_people = sq.Column(sq.Integer, sq.ForeignKey("people.search_vk_id"), nullable=False)
+    id = sq.Column(sq.Integer, primary_key=True)
+    auser_id = sq.Column(sq.Integer, nullable=False)
+    search_vk_id = sq.Column(sq.Integer, nullable=False)
     id_people = sq.Column(sq.Integer, sq.ForeignKey("people.id"), nullable=False)
     people = relationship(People, backref="blacklists")
 
@@ -31,15 +29,12 @@ class Favourite(Base):
     __tablename__ = "favourite"
 
     id = sq.Column(sq.Integer, primary_key=True)
-    auser_id = sq.Column(sq.Integer, nullable=False) #id человека которыый ищет
-    search_vk_id = sq.Column(sq.Integer, nullable=False) #id людей которых мы ищем
+    auser_id = sq.Column(sq.Integer, nullable=False) 
+    search_vk_id = sq.Column(sq.Integer, nullable=False)
     first_name = sq.Column(sq.String(length=40), nullable=False)
     last_name = sq.Column(sq.String(length=40), nullable=False)
     profile_link = sq.Column(sq.String(length=60), nullable=False)
-    first_likes = sq.Column(sq.String(length=60), nullable=False)
-    second_likes = sq.Column(sq.String(length=60), nullable=False)
-    third_likes = sq.Column(sq.String(length=60), nullable=False)
-    # search_vk_id_people = sq.Column(sq.Integer, sq.ForeignKey("people.search_vk_id"), nullable=False)
+    photos_likes = sq.Column(sq.String(length=100), nullable=False)
     id_people = sq.Column(sq.Integer, sq.ForeignKey("people.id"), nullable=False)
     people = relationship(People, backref="favourites")
 
